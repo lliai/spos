2103e36d5f69327f2687130c2825684216939ed6
Chenhe Dong <cheneychenhe@163.com>
2020-01-21 18:11:33 +0800

    delete useless codes

--- multi_best_arch_train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sh distributed_best_arch_train.sh 2 --total_epochs 240 --sched_type 'step'
-sh distributed_best_arch_train.sh 2 --total_epochs 240 --sched_type 'cosine'
-sh distributed_best_arch_train.sh 2 --total_epochs 360 --sched_type 'step'
-sh distributed_best_arch_train.sh 2 --total_epochs 360 --sched_type 'cosine'
